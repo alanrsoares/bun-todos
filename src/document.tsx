65c247da694ee2d2553f51f8fec224f04858dac4
Alan Soares <alanrsoars@gmail.com>
2024-02-16 20:04:52 +1300

    chore: cleanup

--- src/document.tsx
+++ src/document.tsx
@@ -23,27 +23,24 @@ const theme = JSON.stringify(dark, null, 2);
 
 const CLERK_BLOCK = `
 <script>
-  // Get this URL and Publishable Key from the Clerk Dashboard
   const clerkPublishableKey = "${process.env.CLERK_PUBLISHABLE_KEY}";
   const frontendApi = 'classic-termite-73.clerk.accounts.dev';
   const version = '@latest'; // Set to appropriate version
- 
-  // Creates asynchronous script
   const script = document.createElement('script');
+  
   script.setAttribute('data-clerk-frontend-api', frontendApi);
   script.setAttribute('data-clerk-publishable-key', clerkPublishableKey);
   script.async = true;
   script.src = \`https://\${frontendApi}/npm/@clerk/clerk-js\${version}/dist/clerk.browser.js\`;
  
-  // Adds listener to initialize ClerkJS after it's loaded
   script.addEventListener('load', async function () {
     await window.Clerk.load({
-      // Set load options here...
       appearance: {
         baseTheme: ${theme},
       }
     });
   });
+
   document.body.appendChild(script);
 </script>
 `;
